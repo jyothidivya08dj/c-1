822322460fc91fe486655c80cfae9ea6b856cd4c
jyothidivya08dj <jyothidivya08.dj@gmail.com>
2025-12-10 19:49:00 +0530

    Create trace.cpp
    basic

---
+++ trace.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a[100][100],n,i,j,sum=0;
+	printf("\n enter array size");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			printf("\n enter value at a[%d][%d]position",i,j);
+			scanf("%d",&a[i][j]);
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		sum=sum+a[i][i];
+	}  
+	printf("\n sum=%d",sum);
+	return 0;
+}
+
